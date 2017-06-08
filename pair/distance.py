5a846297941c3703f97f295dc413992e0013a939
Jianqin Wang <jianqinwang94@gmail.com>
2017-06-08 10:42:20 -0700

    add input error catching

--- pair/distance.py
+++ pair/distance.py
@@ -11,10 +11,19 @@ def MinDistance2D(coords=[]):
     i = len(coords)
     tempmin = -1
 
+    if (i <= 1):
+        print("Please input at least a pair of coordinates")
+        return
+    if (type(coords[0]) is not list and type(coords[0]) is not tuple):
+        print("Please give a list of lists (x, y coords) or list of tuples")
+        return
     for first in range(i):
         for second in range(first + 1, i):
-            temp = ((coords[first][0] - coords[second][0])**2 +
-                    (coords[first][1] - coords[second][1])**2)
+            if (len(coords[first]) != 2 or len(coords[second]) != 2):
+                print("Please make sure your input is correct for coordinates")
+                return
+            temp = (abs(coords[first][0] - coords[second][0]) +
+                    abs(coords[first][1] - coords[second][1]))
             if (tempmin < 0 or tempmin > temp):
                 tempmin = temp
                 coord1 = coords[first]
@@ -23,7 +32,9 @@ def MinDistance2D(coords=[]):
     return (coord1, coord2)
 
 MinDistance2D([(0, 0), (1, 5), (3, 2), (5, 6)])
-
+MinDistance2D("TEST")
+MinDistance2D([(0, 0)])
+MinDistance2D(["one", "two", "three"])
 
 def MinDistance3D(coords=[]):
     """
@@ -32,11 +43,20 @@ def MinDistance3D(coords=[]):
     i = len(coords)
     tempmin = -1
 
+    if (i <= 1):
+        print("Please input at least a pair of coordinates")
+        return
+    if (type(coords[0]) is not list and type(coords[0]) is not tuple):
+        print("Please give a list of lists (x, y, z coords) or list of tuples")
+        return
     for first in range(i):
         for second in range(first + 1, i):
-            temp = ((coords[first][0] - coords[second][0])**2 +
-                    (coords[first][1] - coords[second][1])**2 +
-                    (coords[first][2] - coords[second][2])**2)
+            if (len(coords[first]) != 3 or len(coords[second]) != 3):
+                print("Please make sure your input is correct for coordinates")
+                return
+            temp = (abs(coords[first][0] - coords[second][0]) +
+                    abs(coords[first][1] - coords[second][1]) +
+                    abs(coords[first][2] - coords[second][2]))
             if (tempmin < 0 or tempmin > temp):
                 tempmin = temp
                 coord1 = coords[first]
@@ -45,3 +65,6 @@ def MinDistance3D(coords=[]):
     return (coord1, coord2)
 
 MinDistance3D([(0, 0, 0), (1, 1, 1), (1, 2, 1), (2, 1, 3)])
+MinDistance2D("Test")
+MinDistance2D([(9, 9, 9)])
+MinDistance2D(["one", "two", "three"])
